bc7c8ae269d2ea3e7904c705683c6886b14345d5
alsdn82 <vhrcnsehr@naver.com>
2021-10-12 12:40:43 +0900

    pyweb

--- pybo/urls.py
+++ pybo/urls.py
@@ -4,6 +4,11 @@ from . import views
 app_name = 'pybo'
 
 urlpatterns = [
-    path('', views.index, name='index'),  # 127.0.0.1:8000/pybo/
-    path('<int:question_id>/', views.detail, name='detail'),  # 127.0.0.1:8000/pybo/1/
+    path('', views.index, name='index'),
+    # 127.0.0.1:8000/pybo/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # 127.0.0.1:8000/pybo/1/
+    path('question/create', views.question_create, name="question_create"),
+    # 127.0.0.1:8000/pybo/question/create
+
 ]
--- pybo/views.py
+++ pybo/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.utils import timezone
 
+from pybo.forms import QuestionForm
 from pybo.models import Question
 
 # 전체 목록 조회
@@ -15,3 +17,16 @@ def detail(request, question_id):
     question = Question.objects.get(id=question_id)
     context = {'question': question}
     return render(request, 'pybo/detail.html', context)
+
+def question_create(request):
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)  # 임시 저장
+            question.create_date = timezone.now() # 등록일
+            question.save()  # 실제 저장
+            return redirect('pybo:index')
+    else:       # request.method == 'GET':
+        form = QuestionForm()
+    context = {'form': form}
+    return render(request, 'pybo/question_form.html', context)
