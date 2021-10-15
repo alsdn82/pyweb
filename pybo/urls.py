9a15bc203bebf3c2e0e9b829f69fe418a034a812
alsdn82 <vhrcnsehr@naver.com>
2021-10-15 13:16:05 +0900

    pyweb

--- pybo/urls.py
+++ pybo/urls.py
@@ -4,7 +4,10 @@ from . import views
 app_name = 'pybo'
 
 urlpatterns = [
+    # 127.0.0.1:8000/pybo/
     path('', views.index, name='index'),
+    # 127.0.0.1:8000/board/
+    path('board/', views.board, name='board'),
     # 127.0.0.1:8000/pybo/
     path('<int:question_id>/', views.detail, name='detail'),
     # 127.0.0.1:8000/pybo/1/ 질문 등록
@@ -12,6 +15,8 @@ urlpatterns = [
     # 127.0.0.1:8000/pybo/question/create  # 답변 등록
     path('answer/create/<int:question_id>/', views.answer_create, name="answer_create"),
     # 질문 수정
-    path('question/modify/<int:question_id>/', views.question_modify, name='question_modify')
+    path('question/modify/<int:question_id>/', views.question_modify, name='question_modify'),
+    # 질문 삭제
+    path('question/delete/<int:question_id>', views.question_delete, name='question_delete'),
 
 ]
