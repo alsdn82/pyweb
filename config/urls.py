b56641877c901d6f475a9b129a77afb7262987e9
alsdn82 <vhrcnsehr@naver.com>
2021-10-18 13:19:47 +0900

    pyweb

--- config/urls.py
+++ config/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('', views.index),
     path('pybo/', include('pybo.urls')),    # 127.0.0.1:8000/pybo/
     path('common/', include('common.urls')),  # 127.0.0.1:8000/common/
-
+    path('poll/', include('poll.urls'))
 ]
