7656e815545e8a602e6f856a47492747844388b8
alsdn82 <vhrcnsehr@naver.com>
2021-10-19 13:21:09 +0900

    pyweb

--- pybo/urls.py
+++ pybo/urls.py
@@ -1,22 +1,27 @@
 from django.urls import path
 from . import views
+from .views import base_views, question_views, answer_views
 
 app_name = 'pybo'
 
 urlpatterns = [
+    # base_views
     # 127.0.0.1:8000/pybo/
-    path('', views.index, name='index'),
+    path('', base_views.index, name='index'),
     # 127.0.0.1:8000/board/
-    path('board/', views.board, name='board'),
+    path('board/', base_views.board, name='board'),
     # 127.0.0.1:8000/pybo/
-    path('<int:question_id>/', views.detail, name='detail'),
-    # 127.0.0.1:8000/pybo/1/ 질문 등록
-    path('question/create/', views.question_create, name="question_create"),
-    # 127.0.0.1:8000/pybo/question/create  # 답변 등록
-    path('answer/create/<int:question_id>/', views.answer_create, name="answer_create"),
-    # 질문 수정
-    path('question/modify/<int:question_id>/', views.question_modify, name='question_modify'),
-    # 질문 삭제
-    path('question/delete/<int:question_id>', views.question_delete, name='question_delete'),
+    path('<int:question_id>/', base_views.detail, name='detail'),
 
+    # question_view
+    # 127.0.0.1:8000/pybo/1/ 질문 등록
+    path('question/create/', question_views.question_create, name="question_create"),
+    # 질문 수정
+    path('question/modify/<int:question_id>/', question_views.question_modify, name='question_modify'),
+    # 질문 삭제
+    path('question/delete/<int:question_id>', question_views.question_delete, name='question_delete'),
+
+    # answer_view
+    # 답변 등록
+    path('answer/create/<int:question_id>/', answer_views.answer_create, name="answer_create"),
 ]
--- pybo/views/question_views.py
+++ pybo/views/question_views.py
@@ -8,41 +8,6 @@ from django.utils import timezone
 from pybo.forms import QuestionForm, AnswerForm
 from pybo.models import Question
 
-def index(request):
-    return render(request, 'pybo/index.html')
-
-
-# 전체 목록 조회
-def board(request):
-
-    # 페이지 - 127.0.0.1:8000/pybo/?page=1
-    page = request.GET.get('page', '1')
-    kw = request.GET.get('kw', '')
-
-    # 조회
-    question_list = Question.objects.order_by('-create_date')  # 내림차순 정렬
-    if kw:
-        question_list = question_list.filter(
-            Q(subject__icontains=kw) |      # 제목 검색
-            Q(content_icontains=kw) |      # 내용 검색
-            Q(answer__content__icontains=kw) |
-            Q(author__username__icontains=kw) |  # 질문 글쓴이 검색
-            Q(answer__author__username__icontains=kw)  # 답변 글쓴이
-        ).distinct()   # 중복 제거한 유일한 내용
-
-    paginator = Paginator(question_list, 10)
-    page_obj = paginator.get_page(page)
-
-    context = {'question_list': page_obj, 'page': page, 'kw': kw}
-    return render(request, 'pybo/question_list.html', context)
-
-# 상세 페이지 조회
-def detail(request, question_id):
-    # question = Question.objects.get(id=question_id)
-    question = get_object_or_404(Question, pk=question_id)
-    context = {'question': question}
-    return render(request, 'pybo/detail.html', context)
-
 # 질문 등록
 @login_required(login_url='common:login')
 def question_create(request):
@@ -58,25 +23,6 @@ def question_create(request):
         form = QuestionForm()
     context = {'form': form}
     return render(request, 'pybo/question_form.html', context)
-
-# 답변 등록
-@login_required(login_url='common:login')
-def answer_create(request, question_id):
-    # question = Question.objects.get(id=question_id)  # 질문 1개 가져오기
-    question = get_object_or_404(Question, pk=question_id)
-    if request.method == "POST":
-        form = AnswerForm(request.POST)
-        if form.is_valid():
-            answer = form.save(commit=False)
-            answer.author = request.user
-            answer.create_date = timezone.now()
-            answer.question = question  # 질문을 답변에 저장
-            form.save()
-            return redirect('pybo:detail', question_id=question.id)
-    else:
-        form = AnswerForm()
-    context = {'form': form}
-    return render(request, 'pybo:detail', context)
 
 # 질문 수정
 @login_required(login_url='common:login')
